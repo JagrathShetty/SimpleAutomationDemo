c11903e0e78c46ccf175cddcabffda885ea11d8f
Dell <Dell@MRIDULA-KEG9N97>
2024-05-15 15:57:16 +0530

    greetings added again

--- AutomationPratice/src/test/java/salutation/Greetings.java
+++ AutomationPratice/src/test/java/salutation/Greetings.java
@@ -3,6 +3,6 @@ package salutation;
 public class Greetings {
 public static void main(String[] args) {
 	
-	System.out.println("Hello Automation Lead..!");
+	System.out.println("Hi...Hello Automation Lead..!");
 }
 }
