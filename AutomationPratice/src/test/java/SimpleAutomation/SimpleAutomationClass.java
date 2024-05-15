14b0452bddd4ab7544cf34145a78ea849adad61b
Jagra <Jagra@JagrathShetty>
2024-05-15 15:44:27 +0530

    SimpleAutomationClass added

---
+++ AutomationPratice/src/test/java/SimpleAutomation/SimpleAutomationClass.java
@@ -0,0 +1,10 @@
+package SimpleAutomation;
+
+public class SimpleAutomationClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hi team member i am your Automation Lead");
+
+	}
+
+}
